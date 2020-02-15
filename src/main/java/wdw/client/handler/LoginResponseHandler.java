f71d3dc232f96ec0f63ac036b413c248954d0771
wdw87 <694439986@qq.com>
2020-02-15 17:02:33 +0800

    构建客户端与服务端的pipline

---
+++ src/main/java/wdw/client/handler/LoginResponseHandler.java
@@ -0,0 +1,37 @@
+package wdw.client.handler;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import wdw.protocal.request.LoginRequestPacket;
+import wdw.protocal.response.LoginResponsePacket;
+import wdw.util.LoginUtil;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class LoginResponseHandler extends SimpleChannelInboundHandler<LoginResponsePacket> {
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+
+        LoginRequestPacket loginRequestPacket = new LoginRequestPacket();
+
+        loginRequestPacket.setUesrId(UUID.randomUUID().toString());
+        loginRequestPacket.setUsername("wdw");
+        loginRequestPacket.setPassword("3346535");
+
+        ctx.channel().writeAndFlush(loginRequestPacket);
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, LoginResponsePacket loginResponsePacket) throws Exception {
+
+        boolean success = loginResponsePacket.isSuccess();
+        String reason = loginResponsePacket.getReason();
+        if(success){
+            System.out.println(new Date() + ": login success ");
+            LoginUtil.markAsLogin(channelHandlerContext.channel());
+        }else{
+            System.out.println(new Date() + ": login failed, " + reason);
+        }
+    }
+}
